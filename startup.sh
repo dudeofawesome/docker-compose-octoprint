b876b2f56e4eb6d7186903ee1012854c862bb7a1
Louis Orleans <louis@orleans.io>
2020-04-18 03:11:27 -0700

    ✨ add klipper

--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-apk add zlib gphoto2 curl
+apk add zlib gphoto2 curl py-numpy
 curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | BINDIR=/usr/local/bin sh
 
 /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
