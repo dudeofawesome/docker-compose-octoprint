e6de3b316bec46a3ddbd5375f2cbe6a797fe5631
Louis Orleans <louis@orleans.io>
2022-01-07 02:43:42 -0800

    🔥 remove unused startup.sh

--- startup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-apk add zlib gphoto2 curl py-numpy
-curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | BINDIR=/usr/local/bin sh
-
-/usr/bin/supervisord -c /etc/supervisor/supervisord.conf
-
